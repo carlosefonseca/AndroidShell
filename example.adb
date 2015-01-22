722a4f61790f5cc9cae9b71d7b47baa565326767
Carlos Fonseca <carlosefonseca@gmail.com>
2015-01-22 00:07:42 +0000

    Updated example .adb

---
+++ example.adb
@@ -0,0 +1,47 @@
+{
+  "_default": {
+    "activity": "com.example.SplashScreen",
+    "dbname": "database.db",
+    "gplay_dev_acc": "12345678901234567890",
+    "mailto": "John Appleseed <john.appleseed@example.com>, Mary <mary@example.com>",
+    "redmine": "project-name-on-redmine",
+    "slack": "channel-name-on-slack"
+  },
+  "_gradle": "FolderWhereBuildDotGradleWithFlavorsIs",
+  "flavor1": {
+    "package": "com.example.app.flavor1"
+  },
+  "flavor2": {
+    "package": "com.example.app.flavor2"
+  },
+  "barcelona": {
+    "package": "pt.beware.mysight.barcelona"
+  },
+  "beware": {
+    "package": "pt.beware.mysight.beware"
+  },
+  "citytourslx": {
+    "package": "pt.beware.mysight.citytourslx"
+  },
+  "dnch": {
+    "package": "pt.beware.mysight.dnch"
+  },
+  "granada": {
+    "package": "pt.beware.mysight.granada"
+  },
+  "lisboa": {
+    "package": "pt.beware.mysight.lisboa"
+  },
+  "london": {
+    "package": "pt.beware.mysight.london"
+  },
+  "madrid": {
+    "package": "pt.beware.mysight.madrid"
+  },
+  "miami": {
+    "package": "pt.beware.mysight.miami"
+  },
+  "sf": {
+    "package": "pt.beware.mysight.sf"
+  }
+}
